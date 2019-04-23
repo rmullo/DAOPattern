670bbe4741eb4a02b086fc67e1c3e9211f908554
Rmullo <Rmullo@20.20.0.166>
2019-04-23 08:11:44 -0300

    ajustes

--- DAOPattern/src/model/CityDAO.java
+++ DAOPattern/src/model/CityDAO.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.mysql.jdbc.Connection;
+//import com.mysql.jdbc.Connection; //Windows comentado
 
 import config.ConnectionFactory;
 
