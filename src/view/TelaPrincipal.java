4f2557e6de93b2c9e6f349b14bf341b847b5a433
romim <romimpereira@gmail.com>
2023-05-03 15:32:29 -0300

    Correção de edição de cidade

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -6,6 +6,7 @@ package view;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 import javax.swing.table.TableModel;
 import model.City;
 import model.CityDAO;
@@ -18,32 +19,20 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
     //Criar Objeto para prover conexão com o banco de dados
     CityDAO cityDAO = new CityDAO();
-    
+    TableModel model;
     /**
      * Creates new form TelaPrincipal
      */
     public TelaPrincipal() {
         initComponents();
         
-        TableModel model = tableCidades.getModel();
+        model = tableCidades.getModel();
         
-        List<City> cidades = cityDAO.readAll();
-
-        // Itera sobre a lista de cidades e adiciona cada uma como uma nova linha na tabela
-        int i = 0;
-        for (City cidade : cidades) {
-            model.setValueAt(cidade.getId(), i, 0);
-            model.setValueAt(cidade.getName(), i, 1);
-            model.setValueAt(cidade.getCountryCode(), i, 2);
-            model.setValueAt(cidade.getDistrict(), i, 3);
-            model.setValueAt(cidade.getPopulation(), i, 4);
-            
-            
-            i++;
-        }
+        this.updateTable();
         
     }
 
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -57,6 +46,10 @@ public class TelaPrincipal extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
         tableCidades = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -78,6 +71,12 @@ public class TelaPrincipal extends javax.swing.JFrame {
                 {null, null, null, null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
                 {null, null, null, null, null}
             },
             new String [] {
@@ -86,26 +85,105 @@ public class TelaPrincipal extends javax.swing.JFrame {
         ));
         jScrollPane2.setViewportView(tableCidades);
 
+        jLabel1.setText("CIDADES");
+
+        jButton1.setText("Adicionar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Editar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Apagar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(46, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 556, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(43, 43, 43))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(292, 292, 292)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(42, 42, 42)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jButton1)
+                                .addGap(29, 29, 29)
+                                .addComponent(jButton2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButton3))
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 556, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(47, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(80, Short.MAX_VALUE)
+                .addContainerGap(37, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3))
+                .addGap(9, 9, 9))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        int linha = tableCidades.getSelectedRow();
+        int coluna = 0;
+        
+        if(linha == -1){
+            JOptionPane.showMessageDialog(null, "Selecione uma Cidade");
+        }
+        int id = Integer.parseInt(model.getValueAt(linha, coluna).toString());
+
+        CreateEditCity cEC = new CreateEditCity(id);
+
+        cEC.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+
+        int linha = tableCidades.getSelectedRow();
+        int coluna = 0;
+        
+        if(JOptionPane.showInternalConfirmDialog(null, "Gostaria de Deletar esta Cidade?")==JOptionPane.YES_OPTION){
+            cityDAO.delete(Integer.parseInt(model.getValueAt(linha, coluna).toString()));
+            this.updateTable();
+        }
+                
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        int linha = tableCidades.getSelectedRow();
+        int coluna = 0;
+
+        CreateEditCity cEC = new CreateEditCity();
+            
+        cEC.setVisible(true);
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    
     /**
      * @param args the command line arguments
      */
@@ -142,9 +220,29 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable tableCidades;
     // End of variables declaration//GEN-END:variables
+
+    private void updateTable() {
+        List<City> cidades = cityDAO.readAll();
+
+        // Itera sobre a lista de cidades e adiciona cada uma como uma nova linha na tabela
+        int i = 0;
+        for (City cidade : cidades) {
+            model.setValueAt(cidade.getId(), i, 0);
+            model.setValueAt(cidade.getName(), i, 1);
+            model.setValueAt(cidade.getCountryCode(), i, 2);
+            model.setValueAt(cidade.getDistrict(), i, 3);
+            model.setValueAt(cidade.getPopulation(), i, 4);
+            
+            i++;
+        }
+    }
 }
